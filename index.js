a9d915cd9750574050965f462c744b746dd01d8b
felipe_louzeiro <fl.louzeiro@gmail.com>
2022-01-26 13:01:25 -0300

    change phrase

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const app = express();
 const PORT = process.env.PORT || 3002
 
 app.get('/', (req, res) => {
-  res.send('Está vivo!!!');
+  res.send('Is a LIIIVE!!!');
 });
 
 app.listen(PORT, () => console.log(`rodando liso na porta ${PORT}`))
+
+// heroku login
+// heroku create meu-1st-deployzin
+// heroku apps
+// heroku git:remote -a meu-1st-deployzin
+// git remote -v
+// git add .
+// git commit -m "make it better"
+// git push heroku master
+//  https://meu-1st-deployzin.herokuapp.com
