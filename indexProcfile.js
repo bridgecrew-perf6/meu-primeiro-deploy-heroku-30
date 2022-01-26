92c11bd5a86fae3ae0fd801fef30a2dd51361f00
felipe_louzeiro <fl.louzeiro@gmail.com>
2022-01-26 13:22:42 -0300

    add procfile 2

--- indexProcfile.js
+++ indexProcfile.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3002
-const TEXT = "Procfile funciona mesmo!"
+const TEXT = process.env.TEXT
 
 app.get('/', (req, res) => {
-  res.send(TEXT);
+  res.send(TEXT, " Procfile funciona mesmo!");
 });
 
 app.listen(PORT, () => console.log(`rodando liso na porta ${PORT}`))
